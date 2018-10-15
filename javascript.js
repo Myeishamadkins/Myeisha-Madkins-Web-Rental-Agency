fb6be20b452e6d826d6fa07bc1988f42f8dcaf09
Myeishamadkins <mmadkins@basecamecodingacademy.org>
2018-10-15 09:37:45 -0500

    makeing a template funtion in javascript.js

---
+++ javascript.js
@@ -0,0 +1,17 @@
+function movieTemplate() {
+    var source = document.getElementById("template").innerHTML;
+    var template = Handlebars.compile(source);
+    var content = "";
+    for (var i = 0; i < PAGE_DATA.moviePosters.length; i++) {
+        content += template({
+            // imageUrl: PAGE_DATA.moviePosters[i].imageUrl,
+            title: PAGE_DATA.moviePosters[i].title,
+            price: PAGE_DATA.moviePosters[i].price,
+            stock: PAGE_DATA.moviePosters[i].stock,
+            description: PAGE_DATA.moviePosters[i].description
+        });
+    }
+    document
+        .querySelector("#placeTemplate")
+        .insertAdjacentHTML("beforeend", content);
+}
